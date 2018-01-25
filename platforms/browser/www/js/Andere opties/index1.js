bf19065b6d4b8f5bfa07635b2b17c22ca7460155
woodvideo <info@woodvideo.nl>
2018-01-25 15:35:03 +0100

    added phonegap stuff

---
+++ platforms/browser/www/js/Andere opties/index1.js
@@ -0,0 +1,62 @@
+document.addEventListener('deviceready', deviceready, false);
+
+var buttomDom;
+var statusDom;
+var fileSystem;
+
+function deviceready() {
+    console.log('dv ready');
+  
+	//step one is to request a file system	
+	window.requestFileSystem(LocalFileSystem.TEMPORARY, 0, 
+		function(fs) {
+			fileSystem = fs;
+	
+			buttonDom = document.querySelector('#startDl');
+			buttonDom.addEventListener('touchend', startDl, false);
+			buttonDom.removeAttribute("disabled");
+	
+			statusDom = document.querySelector('#status');
+		}, function(e) {
+			alert('failed to get fs');
+			alert(JSON.stringify(e));
+		});
+}
+
+function startDl() {
+    buttonDom.setAttribute("disabled","disabled");
+	
+    var ft = new FileTransfer();
+    var uri = encodeURI("http://woodvideo.nl/aandachtspuntaudio/ap-bodyscan-lang.mp3");
+	console.log('Download gestart');
+
+	var downloadPath = fileSystem.root.fullPath + "/ademen.mp3";
+	console.log(fileSystem.root.fullPath);
+
+	ft.onprogress = function(progressEvent) {
+		if (progressEvent.lengthComputable) {
+			var perc = Math.floor(progressEvent.loaded / progressEvent.total * 100);
+			statusDom.innerHTML = perc + "% loaded...";
+		} else {
+			if(statusDom.innerHTML == "") {
+				statusDom.innerHTML = "Loading";
+			} else {
+				statusDom.innerHTML += ".";
+			}
+		}
+	};
+						
+	ft.download(uri, downloadPath, 
+	function(entry) {
+		statusDom.innerHTML = "";
+		var media = new Media(entry.fullPath, null, function(e) { alert(JSON.stringify(e));});
+		media.play();
+		
+	}, 
+	function(error) {
+		alert('Crap something went wrong...');	
+	});
+		
+	
+}
+
---
+++ platforms/browser/www/js/index - kopie.js
@@ -0,0 +1,66 @@
+document.addEventListener('deviceready', deviceready, false);
+
+
+function deviceready() {
+    console.log('dv ready');
+	bestandIsDaar();
+}
+
+function DllFill(){
+window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+    console.log('file system open: ' + fs.name);
+    fs.root.getFile('ap-ademruimtemetvriedelijkheid.mp3', { create: true, exclusive: false }, function (fileEntry) {
+        console.log('fileEntry is file? ' + fileEntry.isFile.toString());
+        var oReq = new XMLHttpRequest();
+        // Make sure you add the domain name to the Content-Security-Policy <meta> element.
+        oReq.open("GET", "http://woodvideo.nl/aandachtspuntaudio/ap-ademruimtemetvriedelijkheid.mp3", true);
+        // Define how you want the XHR data to come back
+        oReq.responseType = "blob";
+        oReq.onload = function (oEvent) {
+            var blob = oReq.response; // Note: not oReq.responseText
+            if (blob) {
+                // Create a URL based on the blob
+                var url = window.URL.createObjectURL(blob);
+				console.log(url);
+				localStorage.setItem('adem', url)
+				bestandIsDaar();
+                /* document.getElementById('muziek').src = url; */
+            } else console.error('we didnt get an XHR response!');
+        };
+        oReq.send(null);
+    }, function (err) { console.error('error getting file! ' + err); });
+}, function (err) { console.error('error getting persistent fs! ' + err); });
+}
+
+
+function downloadSuccess(url) {
+					localStorage.setItem(remoteURL, url.toURL());
+					}
+
+
+function bestandIsDaar(){// the check if there is a cached file
+var remoteSrc = "http://woodvideo.nl/aandachtspuntaudio/ap-ademruimtemetvriedelijkheid.mp3";
+var localSrc = localStorage.getItem('adem');
+
+console.log(localSrc)
+if(localSrc === null) {
+    // when there is NO cached version, use remote
+    document.getElementById('muziek').src = remoteSrc;
+	document.getElementById('moetweg').style="display:none";
+	document.getElementById('downloadknop').style="display:block";
+	
+
+} else {
+    // when there IS a cached version, use local 
+	console.log(localSrc);
+    document.getElementById('muziek').src = localSrc;
+	document.getElementById('moetweg').style="display:block";
+	document.getElementById('downloadknop').style="display:none";
+}
+}
+
+
+function moetWeg(){
+	localStorage.removeItem('adem');
+	bestandIsDaar();
+}
