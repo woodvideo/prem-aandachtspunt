d1425b24e4266a79078d4fe83775132561c73620
woodvideo <info@woodvideo.nl>
2018-02-06 13:47:52 +0100

    10

--- www/js/app.js
+++ www/js/app.js
@@ -125,3 +125,22 @@ function fetch(url) {
 		fsError); 
 				
 }
+
+
+
+
+
+function playAudio(id) {
+    var audioElement = document.getElementById(id);
+    var url = audioElement.getAttribute('src');
+    var my_media = new Media(url,
+            // success callback
+             function () { console.log("playAudio():Audio Success"); },
+            // error callback
+             function (err) { console.log("playAudio():Audio Error: " + err); }
+    );
+           // Play audio
+    my_media.play();
+}
+
+
